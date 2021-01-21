e896a78a4be13300008a11b8bb8629c4c5e27f1a
Christian Wolfe <cwolfe.k@gmail.com>
2021-01-21 12:52:46 -0500

    conditional added for unlike to not decrement past 0

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -5,7 +5,7 @@ function PhotoCard(props) {
     return(
         <div className="card-frame">
             <button onClick={(e) => { setCount(count + 1); }}>like</button>
-            <button onClick={(e) => { setCount(count - 1); }}>unlike</button>
+            <button onClick={(e) => { let decCount = count > 0 ? setCount(count - 1) : count}}>unlike</button>
             <img src={props.url} alt={props.alt}/>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
